c5d713333e7d48f084e3ac57d3273f5b482b1180
Julien-p31 <pighini.julien@outlook.fr>
2024-10-27 10:36:37 +0100

    Create SCRIPT-SSH.sh

---
+++ PROJET2/BASH/SCRIPT-SSH.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PASSWORD="Azerty1*"
+PS3="Sur qu'elle client voulez-vous, vous connecter ? "
+options=("Client Ubuntu : 1" "Client Windows 10 Pro : 2" "Quitter : 3")
+
+select choix in "${options[@]}"; do
+    case $REPLY in
+        1)
+            echo "Connexion effectué !"
+            sshpass -p "$PASSWORD" ssh wilder@172.16.10.30      
+           ;;
+
+         2)
+            echo "Connexion effectué !"
+            sshpass -p "$PASSWORD"  ssh wilder@172.16.10.20
+           ;;
+
+          3)
+            echo "Exit "
+            break
+            ;;
+        *)
+            echo "Option invalide, veuillez réessayer !"
+            ;;
+    esac
+done
