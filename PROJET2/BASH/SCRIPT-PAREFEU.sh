65505cac2935cf14a8a9c43b8baff024ad190273
Julien-p31 <pighini.julien@outlook.fr>
2024-10-31 20:25:54 +0100

    Update SCRIPT-PAREFEU.sh

--- PROJET2/BASH/SCRIPT-PAREFEU.sh
+++ PROJET2/BASH/SCRIPT-PAREFEU.sh
@@ -8,7 +8,7 @@ NC='\033[0m' # Aucune couleur
 
 # Boucle while true pour faire un menu sur la gestion du pare-feu
 while true; do
-    echo -e "${GREEN}------ MENU GESTION PARE-FEU ------"
+    echo -e "${GREEN}------ MENU GESTION PARE-FEU ------\n"
     echo -e "${YELLOW}[1]${NC} Définir les règles de pare-feu"
     echo -e "${YELLOW}[2]${NC} Activer le pare-feu"
     echo -e "${YELLOW}[3]${NC} Désactiver le pare-feu"
@@ -23,11 +23,11 @@ while true; do
          1)
 while true; do
     echo -e "${GREEN}------ MENU DES RÈGLES PARE-FEU ------\n"
-    echo -e "${GREEN}[1]${NC} Ajouter une règle"
-    echo -e "${GREEN}[2]${NC} Supprimer une règle"
-    echo -e "${GREEN}[3]${NC} Afficher les règles actuelles"
-    echo -e "${GREEN}[4]${NC} Retour au menu principal"
-    echo -e "${GREEN}Veuillez choisir une option : ${NC}"
+    echo -e "${YELLOW}[1]${NC} Ajouter une règle"
+    echo -e "${YELLOW}[2]${NC} Supprimer une règle"
+    echo -e "${YELLOW}[3]${NC} Afficher les règles actuelles"
+    echo -e "${YELLOW}[4]${NC} Retour au menu principal"
+    echo -e "${YELLOW}Veuillez choisir une option : ${NC}"
     read choix_regles
     
     case $choix_regles in
