b4286aae850511030945e2359fd7314ae248c3a5
Julien-p31 <pighini.julien@outlook.fr>
2024-11-02 10:55:13 +0100

    Update SCRIPT-LOGICIEL.ps1

--- PROJET2/POWERSHELL/SCRIPT-LOGICIEL.ps1
+++ PROJET2/POWERSHELL/SCRIPT-LOGICIEL.ps1
@@ -8,7 +8,7 @@ $NC = "White" # Aucune couleur
 while ($true) {
     Write-Host "------ MENU GESTION LOGICIEL ------`n" -f $GREEN
     Write-Host "[1] " -f $YELLOW -nonewline; Write-Host " Installer un logiciel" -f $NC
-    Write-Host "[2]  -f $YELLOW -nonewline; Write-Host " Désinstaller un logiciel" -f $NC
+    Write-Host "[2] " -f $YELLOW -nonewline; Write-Host " Désinstaller un logiciel" -f $NC
     Write-Host "[3] " -f $YELLOW -nonewline; Write-Host " Retour au menu principal" -f $NC
     Write-Host "[x] " -f $RED -nonewline; Write-Host " Quitter`n" -f $NC
     $choix = Read-Host "Veuillez choisir une option  "
