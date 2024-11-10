dc6d324fad3f515fe2dfe32d2b995ce94590a39f
Julien-p31 <pighini.julien@outlook.fr>
2024-11-10 02:43:22 +0100

    Update SCRIPT-PAREFEU.ps1

--- PROJET2/POWERSHELL/SCRIPT-PAREFEU.ps1
+++ PROJET2/POWERSHELL/SCRIPT-PAREFEU.ps1
@@ -1,46 +1,59 @@
 # Définir les couleurs des variables
 $RED = "Red"
 $GREEN = "Green"
-$YELLOW = "Yellow"
+$CYAN = "Yellow"
+$CYAN = "Cyan"
 $NC = "White" # Aucune couleur
 
 # Fonction pour menu et choix des règles du pare-feu
 function Menu-ReglesPareFeu {
+
+    # Boucle while pour le menu des règles pare-feu
     while ($true) {
-        Write-Host "------ MENU DES RÈGLES PARE-FEU ------`n" -f $GREEN
-        Write-Host "[1] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Ajouter une règle" -f $NC
-        Write-Host "[2] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Supprimer une règle" -f $NC
-        Write-Host "[3] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Afficher les règles actuelles" -f $NC
-        Write-Host "[4] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Retour au menu principal" -f $NC
-        Write-Host "[x] " -ForegroundColor $RED -NoNewline; Write-Host "Quitter`n" -f $NC
+        Write-Host "`n------ MENU DES RÈGLES PARE-FEU ------`n" -f $GREEN
+        Write-Host "[1] " -ForegroundColor $CYAN -NoNewline; Write-Host "Ajouter une règle" -f $NC
+        Write-Host "[2] " -ForegroundColor $CYAN -NoNewline; Write-Host "Supprimer une règle" -f $NC
+        Write-Host "[3] " -ForegroundColor $CYAN -NoNewline; Write-Host "Afficher les règles actuelles" -f $NC
+        Write-Host "[4] " -ForegroundColor $CYAN -NoNewline; Write-Host "Retour au menu principal`n" -f $NC
         $choix_regles = Read-Host "Veuillez choisir une option "
 
+        # Switch pour les choix du menu
         switch ($choix_regles) {
 
             # Ajouter une règle de pare-feu
-            "1" {
-                $regle = Read-Host "Entrer la règle à ajouter" 
-                Write-Host "Règle ajoutée : $regle`n" -f $GREEN
+           "1" {
 
-                $ruleName = "NomDeLaRegle"
-                $ruleDescription = "Description de la règle"
-                $port = "Ex : port 8080"
-                $direction = "Ex : Inbound ou Outbound"
-                $protocol = "Ex : TCP"
-                $action = "Ex : Allow"
+                # Demander les autres paramètres de la règle
+                $ruleName = Read-Host "`nEntrez le nom de la règle "
+                $ruleDescription = Read-Host "Entrez la description de la règle "
+                $protocol = Read-Host "Entrez le protocole (ex:"TCP" ou "UDP") "
+                $port = Read-Host "Entrez le port (ex:"22" ou "80") "
 
-                New-NetFirewallRule -DisplayName $ruleName -Description $ruleDescription -Direction $direction -Protocol $protocol -LocalPort $port -Action $action
+                # Demander à l'utilisateur de saisir la direction
+                $direction = Read-Host "Entrez la direction (Inbound ou Outbound)"
+
+                # Vérifier que la direction est valide
+                if ($direction -ne "Inbound" -and $direction -ne "Outbound") {
+                Write-Host "Direction invalide. Veuillez entrer 'Inbound' ou 'Outbound'."
+                exit
+                }  
+
+                # Créer la règle avec la direction valide
+                New-NetFirewallRule -DisplayName $ruleName -Description $ruleDescription -Direction $direction -Protocol $protocol -LocalPort $port -Action Allow
+                Write-Host "Règle de pare-feu ajoutée : $ruleName" -f $GREEN
+
+                # Afficher les détails de la règle, y compris les ports
+                Get-NetFirewallRule -DisplayName $ruleName | Format-List *
             }
 
             # Supprimer une règle de pare-feu
             "2" {
-                $regle = Read-Host "Entrer la règle à supprimer"
-                Write-Host "Règle supprimée : $regle`n" -f $GREEN
+                $regle = Read-Host "`nEntrer la règle à supprimer"
+                Write-Host "`nRègle supprimée : $regle`n" -f $GREEN
 
-                $ruleName = "NomDeLaRegle"
+                $ruleName = "$regle"
                 Remove-NetFirewallRule -DisplayName $ruleName
             }
-
             # Affiche les règles pare-feu actuelles
             "3" {
                 Write-Host "Règles de pare-feu actuelles :`n" -f $GREEN
@@ -52,12 +65,6 @@ function Menu-ReglesPareFeu {
                 return 
             }
 
-            # Quitte le script
-            "x" {
-                Write-Host "Exit" -f $YELLOW
-                exit
-            }
-
             # Inique si erreur de saisie et relance le script
             default {
                 Write-Host "[Erreur]! Option invalide, veuillez réessayer !`n" -f $RED
@@ -66,16 +73,16 @@ function Menu-ReglesPareFeu {
     }
 }
 
-# Boucle principale pour la gestion du pare-feu
+# Boucle while pour le menu gestion pare-feu
 while ($true) {
-    Write-Host "------ MENU GESTION PARE-FEU ------`n" -f $GREEN
-    Write-Host "[1] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Définir les règles de pare-feu" -f $NC
-    Write-Host "[2] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Activer le pare-feu" -f $NC
-    Write-Host "[3] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Désactiver le pare-feu" -f $NC
-    Write-Host "[4] " -ForegroundColor $YELLOW -NoNewline; Write-Host "Retour au menu principal" -f $NC
-    Write-Host "[x] " -ForegroundColor $RED -NoNewline; Write-Host "Quitter`n" -f $NC
+    Write-Host "`n------ MENU GESTION PARE-FEU ------`n" -f $GREEN
+    Write-Host "[1] " -ForegroundColor $CYAN -NoNewline; Write-Host "Définir les règles de pare-feu" -f $NC
+    Write-Host "[2] " -ForegroundColor $CYAN -NoNewline; Write-Host "Activer le pare-feu" -f $NC
+    Write-Host "[3] " -ForegroundColor $CYAN -NoNewline; Write-Host "Désactiver le pare-feu" -f $NC
+    Write-Host "[4] " -ForegroundColor $CYAN -NoNewline; Write-Host "Retour au menu principal`n" -f $NC
     $choix = Read-Host "Veuillez choisir une option "
 
+    # Switch pour les choix du menu
     switch ($choix) {
         "1" {
             Menu-ReglesPareFeu  # Appelle la fonction pour le menu des règles de pare-feu
@@ -83,31 +90,39 @@ while ($true) {
 
         # Activation du pare-feu
         "2" {
-            $oui = Read-Host "Voulez-vous activer le pare-feu ? oui/non "
+            $oui = Read-Host "Voulez-vous activer le pare-feu ? (oui/non) "
+
+            # Boucle pour que tant que l'entrée n'est pas "oui" ou "non", on redemande une saisie valide
             while ($oui -ne "oui" -and $oui -ne "non") {
                 Write-Host "[ERREUR] Option invalide, veuillez entrer 'oui' ou 'non'." -f $RED
                 $oui = Read-Host "Entrer votre choix  "
             }
+
+            # Si la valeur saisie est "oui" alors le pare-feu s'active sinon si la saisie est "non" alors rien ne se passe
             if ($oui -eq "oui") {
                 Set-NetFirewallProfile -Profile * -Enabled True
-                Write-Host "Le pare-feu a bien été activé.`n" -f $GREEN
+                Write-Host "`nLe pare-feu a bien été activé.`n" -f $GREEN
             } else {
-                Write-Host "Le pare-feu n'a pas été activé.`n" -f $RED
+                Write-Host "`nLe pare-feu n'a pas été activé.`n" -f $RED
             }
         }
 
         # Désactivation du pare-feu
         "3" {
-            $oui = Read-Host "Voulez-vous désactiver le pare-feu ? : oui/non "
+            $oui = Read-Host "Voulez-vous désactiver le pare-feu ? (oui/non) "
+
+            # Boucle pour que tant que l'entrée n'est pas "oui" ou "non", on redemande une saisie valide
             while ($oui -ne "oui" -and $oui -ne "non") {
                 Write-Host "[ERREUR] Option invalide, veuillez entrer 'oui' ou 'non'." -f $RED
                 $oui = Read-Host "Entrer votre choix  "
             }
+
+            # Si la valeur saisie est "oui" alors le pare-feu se désactive sinon si la saisie est "non" alors rien ne se passe
             if ($oui -eq "oui") {
                 Set-NetFirewallProfile -Profile * -Enabled False
-                Write-Host "Le pare-feu a bien été désactivé.`n" -f $GREEN
+                Write-Host "`nLe pare-feu a bien été désactivé.`n" -f $GREEN
             } else {
-                Write-Host "Le pare-feu n'a pas été désactivé.`n" -f $RED
+                Write-Host "`nLe pare-feu n'a pas été désactivé.`n" -f $RED
             }
         }
 
@@ -115,12 +130,6 @@ while ($true) {
         "4" {
             break
             }
-
-        # Quitte le script
-        "x" {
-            Write-Host "Exit" -f $YELLOW
-            exit
-        }
 
         # Inique si erreur de saisie et relance le script
         default {
