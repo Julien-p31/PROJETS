fcd1a2f36abb7b812cc516f6474cddcc5e49615c
Julien-p31 <pighini.julien@outlook.fr>
2024-11-16 08:32:58 +0100

    Update and rename SCRIPT-LOGICIEL.sh to GestionLogiciel.sh

--- PROJET2/BASH/GestionLogiciel.sh
+++ PROJET2/BASH/GestionLogiciel.sh
@@ -13,33 +13,36 @@ while true; do
     echo -e "${CYAN}[1]${NC} Installer un logiciel"
     echo -e "${CYAN}[2]${NC} Désinstaller un logiciel"
     echo -e "${CYAN}[3]${NC} Retour au menu principal\n"
-    read -p "Veuillez choisir une option : " choix
+    echo -e "${GREEN}Veuillez choisir une option : ${NC}"
+    read choix
 
     # Case pour faire un bloc avec les choix correspondants au menu 
     case $choix in
          # Choix pour installer un logiciel  
          1)
-            read -p "$(echo -e "${GREEN}Entrer le nom du logiciel à installer : ${NC}")" logiciel
+            echo -e "\n${GREEN}Entrer le nom du logiciel à installer : ${NC}"
+	    read logiciel
             sudo apt install $logiciel -y
 	    
 	    # Vérification de la procédure d'installation de logiciel
             if [ $? -eq 0 ]; then
                 echo -e "\n${GREEN}*** Le logiciel $logiciel à été installé avec succés ***${NC}\n"
             else
-                echo -e "\n${RED}*** ERREUR : Le logiciel $logiciel n'a pas été trouvé ou n'a pas pu être installé ***${NC}\n"
+                echo -e "\n${RED}*** ERREUR : Le logiciel $logiciel n'a pas été trouvé ou n'a pas pu être installé. ***${NC}\n"
             fi
           ;;
 	  
          # Choix pour désinstaller un logiciel   
          2)
-            read -p "$(echo -e "${GREEN}Entrer le nom du logiciel à désinstaller : ${NC}")" logiciel
+            echo -e "\n${GREEN}Entrer le nom du logiciel à désinstaller : ${NC}"
+            read logiciel
             sudo apt remove "$logiciel*" -y
 
 	    # Vérification de la procédure de désinstallation de logiciel
    	    if [ $? -eq 0 ]; then
 	    	echo -e "\n${GREEN}*** Le logiciel $logiciel à été désinstallé avec succés ***${NC}\n"
             else
-                echo -e "\n${RED}*** ERREUR : Le logiciel $logiciel n'a pas été trouvé ou n'a pas pu être désinstallé ***${NC}\n"
+                echo -e "\n${RED}*** ERREUR : Le logiciel $logiciel n'a pas été trouvé ou n'a pas pu être désinstallé. ***${NC}\n"
             fi
           ;;
 
